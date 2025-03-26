facb0db58827bf39439a850a9b929d3118f81b43
PLEW(ksahwang) <66160034@kmitl.ac.th>
2025-03-26 10:35:53 +0700

    Update tojo.js

--- cell03/ex04/ex03bis/tojo.js
+++ cell03/ex04/ex03bis/tojo.js
@@ -1,34 +1,61 @@
 let ft_list = document.getElementById("ft_list");
- const create = () => {
-   let txt = prompt("Please Enter TODO LIST :");
-   const name = new Date().getTime();
-   document.cookie = `${name}=${txt}`;
-   let node = document.createElement("div");
-   node.onclick = () => {
-     const check = confirm("Do you want to delete");
-     if (check) {
-       node.remove();
-       document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-     }
-   };
-   node.innerHTML = txt;
-   ft_list.prepend(node);
- };
- 
- const check = document.cookie;
- if (check.length > 0) {
-   const cookie = check.split(";");
-   cookie.forEach((element) => {
-     let node = document.createElement("div");
-     node.onclick = () => {
-       const check = confirm("Do you want to delete");
-       if (check) {
-         node.remove();
-         document.cookie =
-           element.split("=")[0] + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-       }
-     };
-     node.innerHTML = element.split("=")[1];
-     ft_list.prepend(node);
-   });
- }
+
+
+const setCookie = (name, value, days) => {
+  let date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = `${name}=${encodeURIComponent(value)}; expires=${date.toUTCString()}; path=/`;
+};
+
+
+const getCookies = () => {
+  let cookies = document.cookie.split("; ");
+  let data = {};
+  cookies.forEach((cookie) => {
+    let [key, value] = cookie.split("=");
+    if (key && value) data[key] = decodeURIComponent(value);
+  });
+  return data;
+};
+
+
+const deleteCookie = (name) => {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+};
+
+
+const create = () => {
+  let txt = prompt("Please Enter TODO LIST:");
+  if (!txt) return;
+
+  const name = new Date().getTime(); 
+  setCookie(name, txt, 365);
+
+  let node = document.createElement("div");
+  node.innerHTML = txt;
+  node.onclick = () => {
+    const check = confirm("Do you want to delete?");
+    if (check) {
+      node.remove();
+      deleteCookie(name);
+    }
+  };
+  ft_list.prepend(node);
+};
+
+
+window.onload = () => {
+  let savedTodos = getCookies();
+  for (let key in savedTodos) {
+    let node = document.createElement("div");
+    node.innerHTML = savedTodos[key];
+    node.onclick = () => {
+      const check = confirm("Do you want to delete?");
+      if (check) {
+        node.remove();
+        deleteCookie(key);
+      }
+    };
+    ft_list.prepend(node);
+  }
+};
